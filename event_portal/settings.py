d3dd63e3022d0fb7bca50bc88b5362d4d1d01c86
SparkTensor <134692583+SparkTensor@users.noreply.github.com>
2025-07-05 11:23:41 +0100

    Update settings.py
    added secret key

--- event_portal/settings.py
+++ event_portal/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=vxt&5793ndz0^g$xc)bo3@0j&^wfr54732nymjb+qs!z=75b('
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
